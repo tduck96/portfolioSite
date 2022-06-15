43d46ecb90a2c1b5b5e3d92061017c514048d8c2
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-06-15 12:36:26 -0500

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Contact.module.css'
+
 const Contact = () => {
+
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [phone, setPhone] = useState('');
+const [message, setMessage] = useState('');
+const [emailObject, setEmailObject] = useState([]);
+
+const submitForm = (e) => {
+
+  setEmailObject({
+    name: name, 
+    email: email,
+    phone: phone,
+    message: message
+  })
+
+}
+console.log(emailObject);
   return (
     <div className = {styles.boxContainer}>
       <h2 className = {styles.contactPhrase}>  GET IN TOUCH</h2>
@@ -9,18 +28,18 @@ const Contact = () => {
       
       <div className = {styles.formHoldit}>
         <ul className = {styles.contactInfo}>
-          <input type = 'text' placeholder = 'Name*' className = {styles.input}></input>
-          <input type = 'text' placeholder = "Email Address*" className = {styles.input}></input>
-          <input type = 'text' placeholder = "Phone Number*" className = {styles.input}></input>
+          <input type = 'text' placeholder = 'Name*' className = {styles.input} onChange={(e) =>  setName(e.target.value)}></input>
+          <input type = 'text' placeholder = "Email Address*" className = {styles.input}onChange={(e) => setEmail(e.target.value)}></input>
+          <input type = 'text' placeholder = "Phone Number*" className = {styles.input} onChange={(e) =>  setPhone(e.target.value)}></input>
        </ul>
 
           <div className = {styles.bodyContainer}>
-          <input type = 'text' placeholder="Any details of information you wish to provide" className = {styles.inputBody}></input>
+          <input type = 'text' placeholder="Any details of information you wish to provide" className = {styles.inputBody} onChange={(e) => setMessage(e.target.value)}></input>
           </div>
           </div>
           
       </form>
-      <button type ='submit' className = {styles.submitButton}>Send</button>
+      <button type ='submit' className = {styles.submitButton} onClick = {submitForm}>Send</button>
     </div>
   )
 }
