986df602fefd405821986b7cdcdcfff6976b2420
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-06-22 13:31:25 -0500

    media queries for home page
    update to responsiveness for landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import styles from './Home.module.css'
-import loadIcon from './downloadicon.png'
 import download from './download2.png'
+// import Project from './Project';
 
 const Home = () => {
   return (
     <div className = {styles.container}>
-    <h1> <i>Web development, </i></h1>
+    <h1> <i>Web development- </i></h1>
       <h3 className = {styles.welcome}> That's what you came here for, right? </h3>
     <p className = {styles.languagelist}> HTML, CSS, JS, React, NODEJS, MYSQL</p>
     <div className = {styles.actionContainer}>
     <a href = '/projects'><button className = {styles.actionButton}>Projects</button></a>
     <a href = '/contact'><button className = {styles.actionButton}>Contact</button></a>
-    <div className = {styles.resumeContainer}>
-    
-    <img src = {download} alt = 'download-resume-icon' className = {styles.loadIcon}></img>
-    <p>resume(pdf)</p>
-    
+    <div>
+    <button className = {styles.actionButtonResume}><img src = {download} alt = 'download-resume-icon' className = {styles.loadIcon}></img>
+    <p>resume(pdf)</p></button>
     </div>
+    
+  
+    
+    
     </div>
     </div>
   )
