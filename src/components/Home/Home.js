7197b2e26a3ff60d6393bf4df8bb21199bd3b3d7
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-06-29 23:06:54 -0500

    refigured mobile reponse

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,15 +8,12 @@ const Home = () => {
     <div className = {styles.container}>
     {/* <h1> <i>WebDev- </i></h1> */}
       <h1 className = {styles.welcome}><i> Web development- That's what you came here for right?</i> </h1>
-      <p className = {styles.languagelist}> Self-taught developer based out of Nashville, TN</p>
-    <p className = {styles.languagelist}>  React, Node, MYSQL, HTML, CSS</p>
+      <p className = {styles.languagelist}> <i>Self-taught developer based out of Nashville, TN</i></p>
+    <p className = {styles.languagelist}> <i> React, Node, MYSQL, HTML, CSS</i></p>
     <div className = {styles.actionContainer}>
     <a href = '/projects'><button className = {styles.actionButton}>Projects</button></a>
     <a href = '/contact'><button className = {styles.actionButton}>Contact</button></a>
-    <div>
-    <button className = {styles.actionButtonResume}><img src = {download} alt = 'download-resume-icon' className = {styles.loadIcon}></img>
-    <p>Resume</p></button>
-    </div>
+    <a href = '/' target = "_blank"><button className = {styles.actionButton}>Resume</button></a>
     </div>
     </div>
   )
