1b36108600d2d5eb5ed2f7ccc7ae616d7a41af76
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-07-02 09:50:35 -0400

    footer styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './Home.module.css'
 import download from './download2.png'
+import linkedin from '/Users/thomasduckworth/portfolio-site/portfolioSite/src/components/assets/linkedin.png';
+import github from '/Users/thomasduckworth/portfolio-site/portfolioSite/src/components/assets/github.png';
 // import Project from './Project';
 
 const Home = () => {
@@ -8,12 +10,16 @@ const Home = () => {
     <div className = {styles.container}>
     {/* <h1> <i>WebDev- </i></h1> */}
       <h1 className = {styles.welcome}><i> Web development- That's what you came here for right?</i> </h1>
-      <p className = {styles.languagelist}> <i>Self-taught developer based out of Nashville, TN</i></p>
+      <p className = {styles.languagelist}> <i> Self-taught developer based out of Nashville, TN</i></p>
     <p className = {styles.languagelist}> <i> React, Node, MYSQL, HTML, CSS</i></p>
     <div className = {styles.actionContainer}>
     <a href = '/projects'><button className = {styles.actionButton}>Projects</button></a>
     <a href = '/contact'><button className = {styles.actionButton}>Contact</button></a>
     <a href = '/' target = "_blank"><button className = {styles.actionButton}>Resume</button></a>
+    </div>
+    <div className = {styles.contactFooter}>
+    <a href = 'https://www.linkedin.com/in/thomasduckworthdev//' target = "_blank"><img src = {linkedin} alt = '' className = {styles.linkedin}></img></a>
+    <a href = 'https://github.com/tduck96' target = "_blank"><img src = {github} alt = '' className = {styles.github}></img></a>
     </div>
     </div>
   )
